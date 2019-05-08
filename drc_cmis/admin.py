a92a26d3f89957a2482e3e29eddbe6391a929bd7
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2019-05-08 14:56:27 +0200

    disabled the config

--- drc_cmis/admin.py
+++ drc_cmis/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from drc.plugins.models import StorageConfig
+# from drc.plugins.models import StorageConfig
 from solo.admin import SingletonModelAdmin
 
 from .models import CMISConfiguration, DRCCMISConnection
@@ -11,7 +11,7 @@ class DRCCMISConnectionAdmin(admin.ModelAdmin):
     search_fields = ('enkelvoudiginformatieobject', )
 
 
-config = StorageConfig.get_solo()
-if config.cmis_storage:
-    admin.site.register(DRCCMISConnection, DRCCMISConnectionAdmin)
-    admin.site.register(CMISConfiguration, SingletonModelAdmin)
+# config = StorageConfig.get_solo()
+# if config.cmis_storage:
+admin.site.register(DRCCMISConnection, DRCCMISConnectionAdmin)
+admin.site.register(CMISConfiguration, SingletonModelAdmin)
