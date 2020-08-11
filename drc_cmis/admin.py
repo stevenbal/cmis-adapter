59f3523c9a9a5d694e0eebc1ce0e0c727a11f049
Joeri Bekker <joeri@maykinmedia.nl>
2020-08-11 15:05:05 +0200

    :lipstick: Updated admin fields.

--- drc_cmis/admin.py
+++ drc_cmis/admin.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from solo.admin import SingletonModelAdmin
 
@@ -7,4 +9,23 @@ from .models import CMISConfig
 
 @admin.register(CMISConfig)
 class CMISConfigAdmin(SingletonModelAdmin):
-    pass
+    readonly_fields = [
+        "cmis_enabled",
+    ]
+    fieldsets = (
+        (_("General"), {"fields": ("cmis_enabled",)}),
+        (
+            _("Configuration"),
+            {"fields": ("client_url", "binding", "time_zone", "base_folder_name")},
+        ),
+        (_("Credentials"), {"fields": ("client_user", "client_password",)}),
+    )
+
+    def cmis_enabled(self, obj):
+        return settings.CMIS_ENABLED
+
+    cmis_enabled.short_description = _("Enabled")
+    cmis_enabled.boolean = True
+
+    def has_change_permission(self, *args, **kwargs):
+        return settings.CMIS_ENABLED
