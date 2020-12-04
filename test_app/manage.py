c8e760c337cd1ebaf8d6e15571824a2b9dae77a0
silvia <silvia@maykinmedia.nl>
2020-12-04 10:23:34 +0100

    :bug: Fixed manage error

--- test_app/manage.py
+++ test_app/manage.py
@@ -2,9 +2,9 @@
 import os
 import sys
 
+from django.core.management import execute_from_command_line
+
 if __name__ == "__main__":
     os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
 
-    from django.core.management import execute_from_command_line
-
-execute_from_command_line(sys.argv)
+    execute_from_command_line(sys.argv)
